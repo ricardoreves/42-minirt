88f15dfb4dfd2ca94dc095742da3af75ef7c8c61
ricardoreves <ricardo.reves@gmail.com>
2022-03-19 00:43:00 +0100

    refactor: function show_parsing_error

--- srcs/error.c
+++ srcs/error.c
@@ -6,22 +6,23 @@
 /*   By: rpinto-r <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/18 18:32:38 by rpinto-r          #+#    #+#             */
-/*   Updated: 2022/03/18 18:54:22 by rpinto-r         ###   ########.fr       */
+/*   Updated: 2022/03/19 00:19:05 by rpinto-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
-void show_parsing_error(char *msg, int lineno)
+int show_parsing_error(char **arr, char *msg, int num)
 {
 	ft_putstr_fd("Error: ", 2);
 	ft_putstr_fd(msg, 2);
-	if (lineno)
+	if (num)
 	{
 		ft_putstr_fd(" [line:", 2);
-		ft_putnbr_fd(lineno, 2);
+		ft_putnbr_fd(num, 2);
 		ft_putstr_fd("]", 2);
 	}
 	ft_putendl_fd("", 2);
-	exit(1);
+	free_array(arr);
+	return (1);
 }
