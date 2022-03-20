9b8c3ea7778cf86fe0f3a9e2510a8f502357efd0
ricardoreves <ricardo.reves@gmail.com>
2022-03-20 06:09:16 +0100

    update: insert hook_init function

--- srcs/window.c
+++ srcs/window.c
@@ -6,7 +6,7 @@
 /*   By: rpinto-r <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/17 23:13:13 by bgoncalv          #+#    #+#             */
-/*   Updated: 2022/03/19 00:25:46 by rpinto-r         ###   ########.fr       */
+/*   Updated: 2022/03/20 05:30:05 by rpinto-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void	rt_init(t_rt *rt, char *path)
 	rt->img.img = mlx_new_image(rt->mlx, WIN_WIDTH, WIN_HEIGHT);
 	rt->img.addr = mlx_get_data_addr(rt->img.img, &rt->img.bits_per_pixel,
 		&rt->img.line_length, &rt->img.endian);
-	rt->objs = NULL;
+	hook_init(rt);
 	if (!rt->img.img)
 		rt_clear(rt);
 }
@@ -34,13 +34,6 @@ void	rt_clear(t_rt *rt)
 	if (rt->mlx)
 		ft_memdel(&rt->mlx);
 	rt_free(rt);
-}
-
-int	close_window(t_rt *rt)
-{
-	rt_clear(rt);
-	exit(EXIT_SUCCESS);
-	return (0);
 }
 
 void rt_free(t_rt *rt)
