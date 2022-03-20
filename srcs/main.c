53c0ec165265711cd5d60b8f6baae260e76d8bac
ricardoreves <ricardo.reves@gmail.com>
2022-03-20 05:10:20 +0100

    remove: hook.c functions

--- srcs/main.c
+++ srcs/main.c
@@ -6,39 +6,11 @@
 /*   By: rpinto-r <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/17 18:31:39 by rpinto-r          #+#    #+#             */
-/*   Updated: 2022/03/19 01:54:55 by rpinto-r         ###   ########.fr       */
+/*   Updated: 2022/03/20 04:56:51 by rpinto-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
-
-int	msg_quit(char *s)
-{
-	ft_putendl_fd(s, 1);
-	return (EXIT_FAILURE);
-}
-
-int	handle_keydown(int key, t_rt *rt)
-{
-	if (key == 53)
-	{
-		close_window(rt);
-		return (0);
-	}
-	return (0);
-}
-
-int	handle_no_event(t_rt *rt)
-{
-	(void) rt;
-	return (0);
-}
-
-void	hook_init(t_rt *rt)
-{
-	mlx_loop_hook(rt->mlx, handle_no_event, rt);
-	mlx_hook(rt->mlx_win, ON_KEYDOWN, 0, handle_keydown, rt);
-}
 
 int	main(int argc, char **argv)
 {
