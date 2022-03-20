1c33e56be88c978e290f429ed512e2e77f63dfc0
ricardoreves <ricardo.reves@gmail.com>
2022-03-20 06:08:41 +0100

    bugfix: fix key Esc and close button

--- srcs/hook.c
+++ srcs/hook.c
@@ -6,7 +6,7 @@
 /*   By: rpinto-r <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/20 04:57:01 by rpinto-r          #+#    #+#             */
-/*   Updated: 2022/03/20 04:58:05 by rpinto-r         ###   ########.fr       */
+/*   Updated: 2022/03/20 06:03:35 by rpinto-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,27 +15,25 @@
 int	msg_quit(char *s)
 {
 	ft_putendl_fd(s, 1);
-	return (EXIT_FAILURE);
+	return (1);
 }
 
 int	handle_keydown(int key, t_rt *rt)
 {
-	if (key == 53)
-	{
+	if (key == KEY_EXIT)
 		close_window(rt);
-		return (0);
-	}
 	return (0);
 }
 
-int	handle_no_event(t_rt *rt)
+int	close_window(t_rt *rt)
 {
-	(void) rt;
+	rt_clear(rt);
+	exit(0);
 	return (0);
 }
 
 void	hook_init(t_rt *rt)
 {
-	mlx_loop_hook(rt->mlx, handle_no_event, rt);
-	mlx_hook(rt->mlx_win, ON_KEYDOWN, 0, handle_keydown, rt);
+	mlx_hook(rt->mlx_win, 2, 1L<<0, handle_keydown, rt);
+	mlx_hook(rt->mlx_win, 17, 0L, close_window, rt);
 }
