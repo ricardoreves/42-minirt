1747834408061b91cb0f6d371c0c6f8985abc44a
ricardoreves <ricardo.reves@gmail.com>
2022-03-20 05:08:52 +0100

    update: call rt_dump

--- srcs/dev.c
+++ srcs/dev.c
@@ -6,7 +6,7 @@
 /*   By: rpinto-r <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/18 13:48:04 by rpinto-r          #+#    #+#             */
-/*   Updated: 2022/03/19 01:14:53 by rpinto-r         ###   ########.fr       */
+/*   Updated: 2022/03/20 01:34:10 by rpinto-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,38 +15,13 @@
 int main(int argc, char **argv)
 {
 	t_rt *rt;
-	t_object *tmp;
 
 	rt = ft_calloc(sizeof(t_rt), 1);
 	if (argc != 2)
 		ft_putendl_fd("Error: please provide a scene file.", 2);
 	else if (open_file(rt, argv[1]))
 	{
-		printf("sadfadsfsadfasfdadsfadsf");
-		if (rt->camera.id)
-		{
-			printf("obj: camera\n");
-			printf("id: %d\n", rt->camera.id);
-			printf("cx: %f\n", rt->camera.coords.x);
-			printf("cy: %f\n", rt->camera.coords.y);
-			printf("cz: %f\n", rt->camera.coords.z);
-			printf("ox: %f\n", rt->camera.orient.x);
-			printf("oy: %f\n", rt->camera.orient.y);
-			printf("oz: %f\n", rt->camera.orient.z);
-			printf("fov: %ld\n", rt->camera.fov);
-		}
-		tmp = rt->objs;
-		while (tmp)
-		{
-			if (tmp->id == id_ambient)
-			{
-				printf("obj: ambient\n");
-				printf("id: %d\n", tmp->id);
-				printf("co: %d\n", tmp->object.ambient.color);
-				printf("li: %f\n", tmp->object.ambient.lighting);
-			}
-			tmp = tmp->next;
-		}
+		rt_dump(rt);
 	}
 	rt_free(rt);
 	return (0);
