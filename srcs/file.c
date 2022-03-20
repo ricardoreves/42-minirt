c085b44be401b26c37b5fec26fb1246545f5d635
ricardoreves <ricardo.reves@gmail.com>
2022-03-20 06:32:29 +0100

    add: check file extension

--- srcs/file.c
+++ srcs/file.c
@@ -6,11 +6,21 @@
 /*   By: rpinto-r <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/19 00:36:59 by rpinto-r          #+#    #+#             */
-/*   Updated: 2022/03/19 00:37:31 by rpinto-r         ###   ########.fr       */
+/*   Updated: 2022/03/20 06:31:28 by rpinto-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
+
+int	 check_file_extension(char *path)
+{
+	int len;
+
+	len = ft_strlen(path) - 3;
+	if (len > 3)
+		return (ft_strncmp(path + len, ".rt", 3) == 0);
+	return (0);
+}
 
 int read_file(t_rt *rt, int fd)
 {
@@ -20,8 +30,9 @@ int read_file(t_rt *rt, int fd)
 
 	num = 0;
 	ret = 0;
-	while (++num)
+	while (ret != 1)
 	{
+		num++;
 		line = get_next_line(fd);
 		if (!line)
 			break;
@@ -38,6 +49,11 @@ int open_file(t_rt *rt, char *path)
 {
 	int fd;
 
+	if (!check_file_extension(path))
+	{
+		ft_putendl_fd("Error: invalid file extension", 2);
+		return (0);
+	}
 	fd = open(path, O_RDONLY);
 	if (fd == -1)
 	{
