9b5c2fe7d23a1969ee0f891fca49fb114118d0dc
ricardoreves <ricardo.reves@gmail.com>
2022-03-20 05:03:58 +0100

    add:  define ERR_CONTAIN_FORBIDDEN_CHAR

--- incs/parsing.h
+++ incs/parsing.h
@@ -6,13 +6,13 @@
 /*   By: rpinto-r <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/18 02:54:29 by rpinto-r          #+#    #+#             */
-/*   Updated: 2022/03/18 02:56:25 by rpinto-r         ###   ########.fr       */
+/*   Updated: 2022/03/20 04:14:14 by rpinto-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PARSING_H
 # define PARSING_H
 
-# define SCENE_CHARSET "ACLsplcy0123456789-,. "
-
+# define SCENE_CHARSET "ACLsplcy0123456789-,. \n"
+# define ERR_CONTAIN_FORBIDDEN_CHAR "contain forbidden character"
 #endif
