eda137b4aeff3465cd145b7214062d10652e0584
ricardoreves <ricardo.reves@gmail.com>
2022-03-21 02:24:30 +0100

    update: rename macro errors

--- srcs/parsing_utils.c
+++ srcs/parsing_utils.c
@@ -6,7 +6,7 @@
 /*   By: rpinto-r <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/19 01:39:44 by rpinto-r          #+#    #+#             */
-/*   Updated: 2022/03/19 01:51:20 by rpinto-r         ###   ########.fr       */
+/*   Updated: 2022/03/21 02:19:23 by rpinto-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,18 @@ int parse_ambient(t_rt *rt, char *line, int num)
 	t_object *obj;
 	t_ambient ambient;
 
+	if (rt->ambient.id)
+		return (show_parsing_error(args, ERR_TOO_MANY_AMBIENTS, num));
 	args = ft_split(line, ' ');
 	if (array_length(args) != 3)
-		return (show_parsing_error(args, ERR_TOO_FEW_OBJ_ARG, num));
+		return (show_parsing_error(args, ERR_INVALID_NB_PARAMS, num));
 	ft_bzero(&ambient, sizeof(t_ambient));
 	ambient.id = id_ambient;
 	if (parse_float(args[1], &ambient.lighting))
 		return (show_parsing_error(args, ERR_NOT_A_FLOAT, num));
 	if (parse_color(args[2], &ambient.color))
-		return (show_parsing_error(args, ERR_TOO_FEW_COLOR_VAL, num));
-	obj = create_object(rt);
-	obj->id = id_ambient;
-	obj->object.ambient = ambient;
+		return (show_parsing_error(args, ERR_INVALID_NB_COLORS, num));
+	rt->ambient.id = id_ambient;
 	free_array(args);
 	return (0);
 }
@@ -40,20 +40,21 @@ int parse_light(t_rt *rt, char *line, int num)
 	t_object *obj;
 	t_light light;
 
+	if (rt->light.id)
+		return (show_parsing_error(args, ERR_TOO_MANY_AMBIENTS, num));
 	args = ft_split(line, ' ');
 	if (array_length(args) != 4)
-		return (show_parsing_error(args, ERR_TOO_FEW_OBJ_ARG, num));
+		return (show_parsing_error(args, ERR_INVALID_NB_PARAMS, num));
 	ft_bzero(&light, sizeof(t_light));
 	light.id = id_light;
 	if (parse_vector(args[1], &light.coords))
-		return (show_parsing_error(args, ERR_TOO_FEW_COORS_VAL, num));
+		return (show_parsing_error(args, ERR_INVALID_NB_COORDS, num));
 	if (parse_float(args[2], &light.brightness))
 		return (show_parsing_error(args, ERR_NOT_A_FLOAT, num));
 	if (parse_color(args[3], &light.color))
-		return (show_parsing_error(args, ERR_TOO_FEW_COLOR_VAL, num));
+		return (show_parsing_error(args, ERR_INVALID_NB_COLORS, num));
 	obj = create_object(rt);
-	obj->id = id_light;
-	obj->object.light = light;
+	rt->light.id = id_light;
 	free_array(args);
 	return (0);
 }
@@ -66,15 +67,15 @@ int parse_plane(t_rt *rt, char *line, int num)
 
 	args = ft_split(line, ' ');
 	if (array_length(args) != 4)
-		return (show_parsing_error(args, ERR_TOO_FEW_OBJ_ARG, num));
+		return (show_parsing_error(args, ERR_INVALID_NB_PARAMS, num));
 	ft_bzero(&plane, sizeof(t_plane));
 	plane.id = id_plane;
 	if (parse_vector(args[1], &plane.coords))
-		return (show_parsing_error(args, ERR_TOO_FEW_COORS_VAL, num));
+		return (show_parsing_error(args, ERR_INVALID_NB_COORDS, num));
 	if (parse_vector(args[2], &plane.orient))
-		return (show_parsing_error(args, ERR_TOO_FEW_ORIENT_VAL, num));
+		return (show_parsing_error(args, ERR_INVALID_NB_ORIENT, num));
 	if (parse_color(args[3], &plane.color))
-		return (show_parsing_error(args, ERR_TOO_FEW_COLOR_VAL, num));
+		return (show_parsing_error(args, ERR_INVALID_NB_COLORS, num));
 	obj = create_object(rt);
 	obj->id = id_plane;
 	obj->object.plane = plane;
@@ -90,15 +91,15 @@ int parse_sphere(t_rt *rt, char *line, int num)
 
 	args = ft_split(line, ' ');
 	if (array_length(args) != 4)
-		return (show_parsing_error(args, ERR_TOO_FEW_OBJ_ARG, num));
+		return (show_parsing_error(args, ERR_INVALID_NB_PARAMS, num));
 	ft_bzero(&sphere, sizeof(t_sphere));
 	sphere.id = id_sphere;
 	if (parse_vector(args[1], &sphere.coords))
-		return (show_parsing_error(args, ERR_TOO_FEW_COORS_VAL, num));
+		return (show_parsing_error(args, ERR_INVALID_NB_COORDS, num));
 	if (parse_float(args[2], &sphere.diameter))
 		return (show_parsing_error(args, ERR_NOT_A_FLOAT, num));
 	if (parse_color(args[3], &sphere.color))
-		return (show_parsing_error(args, ERR_TOO_FEW_COLOR_VAL, num));
+		return (show_parsing_error(args, ERR_INVALID_NB_COLORS, num));
 	obj = create_object(rt);
 	obj->id = id_sphere;
 	obj->object.sphere = sphere;
@@ -114,19 +115,19 @@ int parse_cylinder(t_rt *rt, char *line, int num)
 
 	args = ft_split(line, ' ');
 	if (array_length(args) != 6)
-		return (show_parsing_error(args, ERR_TOO_FEW_OBJ_ARG, num));
+		return (show_parsing_error(args, ERR_INVALID_NB_PARAMS, num));
 	ft_bzero(&cylinder, sizeof(t_cylinder));
 	cylinder.id = id_cylinder;
 	if (parse_vector(args[1], &cylinder.coords))
-		return (show_parsing_error(args, ERR_TOO_FEW_COORS_VAL, num));
+		return (show_parsing_error(args, ERR_INVALID_NB_COORDS, num));
 	if (parse_vector(args[2], &cylinder.orient))
-		return (show_parsing_error(args, ERR_TOO_FEW_ORIENT_VAL, num));
+		return (show_parsing_error(args, ERR_INVALID_NB_ORIENT, num));
 	if (parse_float(args[3], &cylinder.diameter))
 		return (show_parsing_error(args, ERR_NOT_A_FLOAT, num));
 	if (parse_float(args[4], &cylinder.height))
 		return (show_parsing_error(args, ERR_NOT_A_FLOAT, num));
 	if (parse_color(args[5], &cylinder.color))
-		return (show_parsing_error(args, ERR_TOO_FEW_COLOR_VAL, num));
+		return (show_parsing_error(args, ERR_INVALID_NB_COLORS, num));
 	obj = create_object(rt);
 	obj->id = id_cylinder;
 	obj->object.cylinder = cylinder;
