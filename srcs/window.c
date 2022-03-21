d897ac57f81f69ca5ed880e790ae2d2473035a3a
ricardoreves <ricardo.reves@gmail.com>
2022-03-21 14:32:08 +0100

    refactor:  handle_keydown  just call exit_win on Esc

--- srcs/window.c
+++ srcs/window.c
@@ -6,33 +6,29 @@
 /*   By: rpinto-r <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/17 23:13:13 by bgoncalv          #+#    #+#             */
-/*   Updated: 2022/03/20 15:40:25 by rpinto-r         ###   ########.fr       */
+/*   Updated: 2022/03/21 14:30:55 by rpinto-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
+static int	exit_win(t_rt *rt)
+{
+	rt_clear(rt);
+	exit(0);
+	return (0);
+}
+
 static int	handle_keydown(int key, t_rt *rt)
 {
 	if (key == ESCAPE_KEY)
-	{
-		rt_clear(rt);
-		exit(EXIT_SUCCESS);
-		return (0);
-	}
+		exit_win(rt);
 	return (0);
 }
 
 static int	handle_no_event(t_rt *rt)
 {
 	(void) rt;
-	return (0);
-}
-
-static int	exit_win(t_rt *rt)
-{
-	rt_clear(rt);
-	exit(0);
 	return (0);
 }
 
@@ -47,7 +43,7 @@ void	rt_init(t_rt *rt, char *path)
 	if (!rt->img.img)
 		rt_clear(rt);
 	mlx_loop_hook(rt->mlx, handle_no_event, rt);
-	mlx_hook(rt->mlx_win, ON_KEYDOWN, 0, handle_keydown, rt);
+	mlx_hook(rt->mlx_win, ON_KEYDOWN, 1L<<2, handle_keydown, rt);
 	mlx_hook(rt->mlx_win, ON_DESTROY, 0, exit_win, rt);
 }
 
