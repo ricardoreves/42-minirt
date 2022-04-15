1b906bc67e7271063046834d53a610c8c51eb3b4
ricardoreves <ricardo.reves@gmail.com>
2022-04-15 20:36:19 +0200

    add: light_utils.c

---
+++ srcs/utils/light_utils.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   light_utils.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rpinto-r <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/15 18:34:51 by rpinto-r          #+#    #+#             */
+/*   Updated: 2022/04/15 20:24:03 by rpinto-r         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minirt.h"
+
+void push_light(t_light *new_light, t_light **lights)
+{
+	t_light *tmp;
+
+	if (!(*lights))
+		*lights = new_light;
+	else
+	{
+		tmp = *lights;
+		while (tmp->next)
+			tmp = tmp->next;
+		tmp->next = new_light;
+	}
+}
+
+t_light *create_light(t_rt *rt)
+{
+	t_light *new_light;
+
+	new_light = ft_calloc(sizeof(t_light), 1);
+	if (!new_light)
+		return (0);
+	push_light(new_light, &rt->light);
+	return (new_light);
+}
+
+void free_lights(t_light **lights)
+{
+	t_light *tmp;
+	t_light *light;
+
+	light = *lights;
+	while (light)
+	{
+		tmp = light;
+		light = light->next;
+		free(tmp);
+	}
+	*lights = 0;
+}
