2804e4ef211af872d031d27874ba0a5fecf9fc00
ricardoreves <ricardo.reves@gmail.com>
2022-04-21 17:07:05 +0200

    update: MSG_USAGE

--- srcs/main/main.c
+++ srcs/main/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bgoncalv <bgoncalv@student.42lausanne.ch>  +#+  +:+       +#+        */
+/*   By: rpinto-r <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/17 18:31:39 by rpinto-r          #+#    #+#             */
-/*   Updated: 2022/04/20 17:29:28 by bgoncalv         ###   ########.fr       */
+/*   Updated: 2022/04/21 16:08:57 by rpinto-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ int	main(int argc, char **argv)
 
 	rt = ft_calloc(sizeof(t_rt), 1);
 	if (argc != 2)
-		show_error(USAGE_MESSAGE);
+		show_error(MSG_USAGE);
 	else if (open_file(rt, argv[1]))
 	{
 		rt_init(rt, argv[1]);
