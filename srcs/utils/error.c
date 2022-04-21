d3adf81dae459aecd43e2a1f4dfe79342ecef97c
ricardoreves <ricardo.reves@gmail.com>
2022-04-21 17:02:20 +0200

    update: show param number

--- srcs/utils/error.c
+++ srcs/utils/error.c
@@ -3,23 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   error.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bgoncalv <bgoncalv@student.42lausanne.ch>  +#+  +:+       +#+        */
+/*   By: rpinto-r <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/18 18:32:38 by rpinto-r          #+#    #+#             */
-/*   Updated: 2022/04/17 01:20:59 by bgoncalv         ###   ########.fr       */
+/*   Updated: 2022/04/21 14:32:11 by rpinto-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
-int	show_parsing_error(char **arr, char *msg, int num)
+int	show_parsing_error(t_rt *rt, char **arr, char *msg)
 {
 	ft_putstr_fd("Error: ", 2);
 	ft_putstr_fd(msg, 2);
-	if (num)
+	if (rt->lnum)
 	{
 		ft_putstr_fd(" [line:", 2);
-		ft_putnbr_fd(num, 2);
+		ft_putnbr_fd(rt->lnum, 2);
+		ft_putstr_fd(":param:", 2);
+		ft_putnbr_fd(rt->pnum + 1, 2);
 		ft_putstr_fd("]", 2);
 	}
 	ft_putendl_fd("", 2);
