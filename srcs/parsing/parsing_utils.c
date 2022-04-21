1322f5f4f8a342147b1e57da2e6b174a9fa4e6ac
ricardoreves <ricardo.reves@gmail.com>
2022-04-21 22:54:30 +0200

    bugfix: the first color becomes black s there are two colors

--- srcs/parsing/parsing_utils.c
+++ srcs/parsing/parsing_utils.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   parsing_utils2.c                                   :+:      :+:    :+:   */
+/*   parsing_utils.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rpinto-r <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/19 01:37:46 by rpinto-r          #+#    #+#             */
-/*   Updated: 2022/04/20 02:57:58 by rpinto-r         ###   ########.fr       */
+/*   Updated: 2022/04/21 22:49:01 by rpinto-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,68 +14,71 @@
 
 int	parse_vector(char *str, t_vect *vect)
 {
-	char	**tmp;
-	int		ret;
 	int		i;
+	int		ret;
+	char	**nbrs;
 
 	i = -1;
 	ret = 0;
-	tmp = ft_split(str, ',');
-	while (tmp && tmp[++i])
-		if (!is_float(tmp[i]))
+	nbrs = ft_split(str, ',');
+	while (nbrs && nbrs[++i])
+		if (!is_float(nbrs[i]))
 			ret = 1;
-	if (array_length(tmp) != 3)
+	if (array_length(nbrs) != 3)
 		ret = 1;
 	else
 	{
-		vect->x = str_to_float(tmp[0]);
-		vect->y = str_to_float(tmp[1]);
-		vect->z = str_to_float(tmp[2]);
+		vect->x = str_to_float(nbrs[0]);
+		vect->y = str_to_float(nbrs[1]);
+		vect->z = str_to_float(nbrs[2]);
 	}
-	free_array(tmp);
+	free_array(nbrs);
 	return (ret);
 }
 
 int	parse_colors(char *str, t_color *color, t_color *color2)
 {
-	char	**tmp;
-	int		ret;
 	int		i;
+	int		ret;
+	char	**colors;
 
 	i = -1;
 	ret = 0;
-	tmp = ft_split(str, ';');
-	if (array_length(tmp) < 1 || array_length(tmp) > 2)
+	colors = ft_split(str, ';');
+	if (array_length(colors) < 1)
 		ret = 1;
-	if (array_length(tmp) == 1)
-		ret = parse_color(tmp[0], color);
-	if (array_length(tmp) == 2)
-		ret = parse_color(tmp[1], color2);
-	free_array(tmp);
+	while (colors && colors[++i])
+	{
+		if (i == 0)
+			ret = parse_color(colors[0], color);
+		if (i == 1)
+			ret = parse_color(colors[1], color2);
+	}
+	free_array(colors);
 	return (ret);
 }
 
 int	parse_color(char *str, t_color *color)
 {
-	char	**tmp;
-	int		ret;
 	int		i;
+	int		ret;
+	char	**rgb;
 
 	i = -1;
 	ret = 0;
-	tmp = ft_split(str, ',');
-	while (tmp && tmp[++i])
-		if (!is_ulong(tmp[i]))
+	rgb = ft_split(str, ',');
+	while (rgb && rgb[++i])
+		if (!is_ulong(rgb[i]))
 			ret = 1;
-	if (array_length(tmp) != 3)
+	if (array_length(rgb) != 3)
 		ret = 1;
 	else
 	{
-		color->r = (float) str_to_int_color(tmp[0]) / 255;
-		color->g = (float) str_to_int_color(tmp[1]) / 255;
-		color->b = (float) str_to_int_color(tmp[2]) / 255;
+		color->r = (float) str_to_int_color(rgb[0]) / 255;
+		color->g = (float) str_to_int_color(rgb[1]) / 255;
+		color->b = (float) str_to_int_color(rgb[2]) / 255;
 	}
-	free_array(tmp);
+	free_array(rgb);
 	return (ret);
 }
 
